09507af63da6c0bf5335eb3d7388a8a5e130e370
LAHMER Seyyid-Ahmed <seyyidahmed.lahmer@etu.unistra.fr>
2020-05-02 14:27:53 +0200

    qsdqsd

--- client_sources/rest_client.cpp
+++ client_sources/rest_client.cpp
@@ -84,13 +84,11 @@ void rest_client::client_network()  {
             if(ret[3] == true){
                 send_fire_message();
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(22));
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
          }
 
     });
-
-
-
+    
     std::vector<std::shared_ptr<polyClient>> v;
 
     while (true) {
